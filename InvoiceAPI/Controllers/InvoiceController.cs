84a97fb33dde565f4a49975819bf02790447015c
PeterCellar <petocell@gmail.com>
2024-01-21 22:48:38 +0100

    added response codes attributes to action methods, refactoring of code

--- InvoiceAPI/Controllers/InvoiceController.cs
+++ InvoiceAPI/Controllers/InvoiceController.cs
@@ -1,6 +1,7 @@
 using InvoiceAPI.DataHelper;
 using InvoiceAPI.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceAPI.Controllers
@@ -21,52 +22,101 @@ namespace InvoiceAPI.Controllers
         
 
         [HttpPost("Create")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Invoice))]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Invoice>> CreateInvoice(Invoice invoice)
         {
-            var existingInvoice = _sqlDbContext.Invoices.FindAsync(invoice.Uuid);
-            if(existingInvoice.Result != null) { throw new DbUpdateException(); } 
+            try
+            {
+                DataOperations.ValidateInvoiceModel(invoice);
 
-            DataOperations.ValidateData(invoice);
+                _sqlDbContext.Invoices.Add(invoice);
+                await _sqlDbContext.SaveChangesAsync();
 
-            _sqlDbContext.Invoices.Add(invoice);
-            await _sqlDbContext.SaveChangesAsync();    
+            }
+            catch(DbUpdateException)
+            {
+                return Conflict($"Duplicate key or unique constraint violation.\nInvoice with {invoice.Uuid} already exists.");
+            }
+            catch(InvalidDataException ex)
+            {
+                return BadRequest("Invalid invoice data.\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error\n" + ex.Message);
+            }
 
-            return CreatedAtAction(nameof(CreateInvoice), new { id = invoice.Uuid }, invoice);
+            return CreatedAtAction(nameof(CreateInvoice), new { id = invoice.Uuid}, invoice);
         }
 
         [HttpGet("Read/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Invoice))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Invoice>> GetInvoice(Guid id)
         {
             var invoice = await _sqlDbContext.Invoices.FindAsync(id);   
-
-            return invoice == null ? NotFound() : invoice;  
+            return invoice == null ? NotFound() : Ok(invoice);  
         }
 
+
         [HttpPut("Update/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Invoice>> UpdateInvoice(Guid id, UpdateInvoice? invoice)
         {
             var invoiceToUpdate = await _sqlDbContext.Invoices.FindAsync(id);
-            
+            if (invoiceToUpdate == null) { return  NotFound(); }
 
-            DataOperations.UpdateInvoice(ref invoiceToUpdate!, invoice);
-            _sqlDbContext.SaveChanges();
+            if (invoice == null) { return NoContent(); }
 
-            return invoiceToUpdate;
+            try
+            {
+                DataOperations.ValidateUpdateInvoiceModel(invoice);
+                DataOperations.UpdateInvoice(ref invoiceToUpdate!, invoice);
+                _sqlDbContext.SaveChanges();
+            }
+            catch(DbUpdateException)
+            {
+                return Conflict($"Duplicate key or unique constraint violation.\nInvoice with {invoiceToUpdate.Uuid} already exists.");
+            }
+            catch(InvalidDataException ex)
+            {
+                return BadRequest("Invalid invoice data.\n" + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error\n" + ex.Message);
+            }
+
+            return NoContent();
         }
 
         [HttpDelete("Delete/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteInvoice(Guid id)
         {
             var invoiceToDelete = await _sqlDbContext.Invoices.FindAsync(id);
-            
-            if(invoiceToDelete != null) 
+            if(invoiceToDelete == null) { return NotFound(); }
+
+            try
             {
                 _sqlDbContext.Invoices.Remove(invoiceToDelete);
                 _sqlDbContext.SaveChanges();
+
                 return NoContent();
             }
-
-            return NotFound();
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error\n" + ex.Message);
+            }
         }
     }
 }
--- InvoiceAPI/DataHelper/DataOperations.cs
+++ InvoiceAPI/DataHelper/DataOperations.cs
@@ -4,40 +4,43 @@ namespace InvoiceAPI.DataHelper
 {
     public static class DataOperations
     {
-        public static void UpdateInvoice(ref Invoice invoice, UpdateInvoice? updateInvoice)
+        public static void UpdateInvoice(ref Invoice invoice, UpdateInvoice updateInvoice)
         {
-            if (updateInvoice != null)
-            {
-                if (updateInvoice.CreationDate != null) { invoice.CreationDate = updateInvoice.CreationDate; }
+            if (updateInvoice.CreationDate != null) { invoice.CreationDate = updateInvoice.CreationDate; }
 
-                if (updateInvoice.UpdateDate != null) { invoice.UpdateDate = updateInvoice.UpdateDate; }
+            if (updateInvoice.UpdateDate != null) { invoice.UpdateDate = updateInvoice.UpdateDate; }
 
-                if (updateInvoice.Ammount != null) { invoice.Ammount = (double)updateInvoice.Ammount; }
+            if (updateInvoice.Ammount != null) { invoice.Ammount = (double)updateInvoice.Ammount; }
 
-                if (updateInvoice.SupplierFullName != null) { invoice.SupplierFullName = updateInvoice.SupplierFullName; }
+            if (updateInvoice.SupplierFullName != null) { invoice.SupplierFullName = updateInvoice.SupplierFullName; }
 
-                if (updateInvoice.SupplierIco != null) { invoice.SupplierIco = updateInvoice.SupplierIco; }
+            if (updateInvoice.SupplierIco != null) { invoice.SupplierIco = updateInvoice.SupplierIco; }
 
-                if (updateInvoice.PurchaserFullName != null) { invoice.PurchaserFullName = updateInvoice.PurchaserFullName; }
+            if (updateInvoice.PurchaserFullName != null) { invoice.PurchaserFullName = updateInvoice.PurchaserFullName; }
 
-                if (updateInvoice.PurchaserIco != null) { invoice.PurchaserIco = updateInvoice.PurchaserIco; }
+            if (updateInvoice.PurchaserIco != null) { invoice.PurchaserIco = updateInvoice.PurchaserIco; }
 
-                if (updateInvoice.IssueDate != null) { invoice.IssueDate = updateInvoice.IssueDate; }
+            if (updateInvoice.IssueDate != null) { invoice.IssueDate = updateInvoice.IssueDate; }
 
-                if (updateInvoice.DueDate != null) { invoice.DueDate = updateInvoice.DueDate; }
+            if (updateInvoice.DueDate != null) { invoice.DueDate = updateInvoice.DueDate; }
 
-                if (updateInvoice.FulfillmentDate != null) { invoice.FulfillmentDate = updateInvoice.FulfillmentDate; }
+            if (updateInvoice.FulfillmentDate != null) { invoice.FulfillmentDate = updateInvoice.FulfillmentDate; }
 
-                if (updateInvoice.PaymentType != null) { invoice.PaymentType = (Payment)updateInvoice.PaymentType; }
-            }
-
+            if (updateInvoice.PaymentType != null) { invoice.PaymentType = (Payment)updateInvoice.PaymentType; }
         }
 
-        public static void ValidateData(Invoice invoice)
+        public static void ValidateInvoiceModel(Invoice invoice)
         {
             if(!ValidateIco(invoice.SupplierIco!)) { throw new InvalidDataException(); }
 
             if(!ValidateIco(invoice.PurchaserIco!)) { throw new InvalidDataException(); }
+        }
+
+        public static void ValidateUpdateInvoiceModel(UpdateInvoice invoice)
+        {
+            if (invoice.SupplierIco != null && !ValidateIco(invoice.SupplierIco)) { throw new InvalidDataException(); }
+
+            if (invoice.PurchaserIco != null && !ValidateIco(invoice.PurchaserIco!)) { throw new InvalidDataException(); }
         }
 
         public static bool ValidateIco(string ico)
